8425009ad949cb11e18f509067f65e6ce3778599
salah bakhash <salahb170@gmail.com>
2025-04-20 19:56:28 +0300

    remove unrelatid files

--- android/app/.cxx/Debug/3x286b49/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/salah/.gradle/caches/8.10.2/transforms/90773311d8af38d2592dba0ae56fb603/transformed/jetified-hermes-android-0.77.2-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/salah/.gradle/caches/8.10.2/transforms/90773311d8af38d2592dba0ae56fb603/transformed/jetified-hermes-android-0.77.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/3x286b49/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/salah/.gradle/caches/8.10.2/transforms/f11952b525f3a5e4d0e94e235f377d46/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/salah/.gradle/caches/8.10.2/transforms/f11952b525f3a5e4d0e94e235f377d46/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
